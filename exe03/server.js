52abd8ea73ca624e6a73b41ec1d9b79d5f6ac149
jori <nanosix@gmail.com>
2018-01-12 13:47:27 +0100

    excercise04

--- exe03/server.js
+++ exe03/server.js
@@ -2,20 +2,40 @@
 
 const http = require('http')
 const fs = require('fs')
+const values = []
+const sitesToHack = ['http://www.mons.be', 'http://www.triptyk.eu', 'http://www.pass.be']
+let sitesLoaded = 0
 
-
-http.get ('http://www.mons.be', res => {
-    res.setEncoding('utf-8')
-    let body =''
-    res.on('data',donnee=>{
-        body += donnee
-    })
-    res.on('end',()=>{
-        console.log('end')
-        fs.writeFile('index.html',body,(err)=>{
-            if (err) console.log(err)
-            console.log('file generated')
+const getPageFromSiteAndStock = (url) => {
+    http.get(url,res=>{
+        let body = ''
+        res.on('data',data=>{
+            body += data
+        })
+        res.on('end',()=>{
+            console.log(`end of loading ${url}`)
+            values.push(body)
+            checkFinishedProcess()
         })
     })
+}
 
-})
+const checkFinishedProcess = () => {
+    sitesLoaded++
+    if(sitesLoaded === sitesToHack.length){
+        console.log('all page are saved in the file')
+        const tmpStr = values.reduce((prev,current)=>{
+            return `${prev}\n${current}`
+        })
+        fs.writeFile('results.html', tmpStr,(err)=>{
+            if (err) console.log(err)
+            console.log('all sites are saved')
+        })
+    }
+}
+
+const init = ()=>{
+    sitesToHack.map(item => getPageFromSiteAndStock(item))
+}
+
+init()
