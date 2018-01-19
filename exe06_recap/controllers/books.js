43a6427e94cd294641834c02276b57b633bf3c25
nanosix <nanosix@gmail.com>
2018-01-19 09:20:34 +0100

    exe06

--- exe06_recap/controllers/books.js
+++ exe06_recap/controllers/books.js
@@ -2,15 +2,43 @@ const fs = require('fs')
 const promisify = require('es6-promisify')
 
 const read = promisify(fs.readFile)
+const write = promisify(fs.writeFile)
 
 module.exports = (req, res) => {
-    let templatePromise = read(`${process.cwd()}/models/books.json`)   // lire le json dans models ( et le chemin relatif avec process.cwd)
-    templatePromise.then(json=>{
-        res.setHeader('Content-type', 'application/json')
-        res.end(json)  
-    }).catch(e =>{
-        console.log(e)
-    })
+    if ( req.method === 'GET') {
+        let templatePromise = read(`${process.cwd()}/models/books.json`)   // lire le json dans models ( et le chemin relatif avec process.cwd)
+        templatePromise.then(json=>{
+            res.setHeader('Content-type', 'application/json')
+            res.end(json)  
+        }).catch(e =>{
+            console.log(e)
+        })
+
+    } else if ( req.method === 'POST') {
+        let formData = ''
+        req.on('data', ((data)=> {  // quand il a des données il les rajoute dans mon formdata
+            formData += data
+        }))
+        req.on('end', ()=> {  // quand il arrive a la fin 
+            console.log('write to file', formData)
+            read(`${process.cwd()}/models/books.json`)
+                .then((data)=> {
+
+                    let tempArr = JSON.parse(data)
+                    let jsonObject = JSON.parse(formData)
+                    tempArr.push(jsonObject)
+
+                    write(`${process.cwd()}/models/books.json`,JSON.stringify(tempArr))
+                        .then(()=>{
+                            console.log(tempArr)
+                            res.end(JSON.stringify(tempArr))
+                        })
+                }).catch(e=>{console.log(e)})
+
+            
+        })
+    }
+
 }
 
 
